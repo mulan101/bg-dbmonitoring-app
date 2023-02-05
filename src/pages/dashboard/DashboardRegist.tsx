7a3f20296551805beb44299fbee223b2081447c4
jeho.han <mulan101@gmail.com>
2023-02-05 22:50:09 +0900

    commit 1

--- src/pages/dashboard/DashboardRegist.tsx
+++ src/pages/dashboard/DashboardRegist.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Button, Field, Input, FieldSet } from '@grafana/ui';
+import { config } from '@grafana/runtime';
 //import { useContext } from 'react';
 //import PanelContext from 'PluginContext';
 
@@ -8,7 +9,6 @@ const divStyle = {
   }
 
 export const DashboardRegist = () => {
-
   function onSubmit() {
     alert(11111)
     //const { userDetails } = useContext(PanelContext);
@@ -18,9 +18,10 @@ export const DashboardRegist = () => {
     <div style={divStyle}>
       <h3>대시보드 등록</h3>
       <form onSubmit={onSubmit}>
+            <Input type="hidden" name="custId" id="custId"  value={config.bootData.user.orgId}/>
             <FieldSet> 
-            <Field label="대시보드명">
-                <Input name="dashboardId" id="dashboardId" required/>
+              <Field label="대시보드명">
+                <Input name="dashboardName" id="dashboardName" required/>
               </Field>
               <Field label="대시보드 아이디">
                 <Input name="dashboardId" id="dashboardId" required/>
